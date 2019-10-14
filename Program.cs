93e4a8976d5e8ea58b7637a0449efe5a7f47e83d
Carl Barringer <cbarringer@Carls-MacBook-Pro.local>
2019-10-14 14:56:45 -0500

    final commit

--- Program.cs
+++ Program.cs
@@ -6,6 +6,15 @@ using LinqIntro;
 
 namespace linq
 {
+
+    public class Customer
+        {
+            public string Name { get; set; }
+            public double Balance { get; set; }
+            public string Bank { get; set; }
+        }
+
+
     class Program
     {
         static void Main(string[] args)
@@ -141,6 +150,89 @@ namespace linq
             {
                 2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
             };
+            var TotalPurchases = purchases.Sum();
+            Console.WriteLine(TotalPurchases);
+            Console.WriteLine();
+            // What is our most expensive product?
+            List<double> prices = new List<double>()
+            {
+                879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
+            };
+            var MaxPrices = prices.Max();
+            Console.WriteLine(MaxPrices);
+            Console.WriteLine();
+            /*
+                Store each number in the following List until a perfect square
+                is detected.
+
+                Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
+            */
+            List<int> wheresSquaredo = new List<int>()
+            {
+                66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
+            };
+            List<int> HeresSquaredo = wheresSquaredo.Where(SquareNumber => 
+                
+                Math.Sqrt(SquareNumber) % 1 == 0).ToList();
+                
+
+            {
+                double sqrt = Math.Sqrt(num);
+                return sqrt % 1 != 0;
+
+        
+            }
+            //the join problem
+            var reportItems = customers
+                .Where(c => c.Balance >= 1000000)
+                .Join(
+                    banks,
+                Customer => Customer.Bank,
+                bank => bank.symbol,
+                (Customer, bank) => 
+                {
+                    report new ReportItem
+                    {CustomerName = Customer.Name,
+                    BankName = bank.Name
+                    };
+
+                })
+
+
+            var report = customers
+            .Where(c => c.Balance >= 10000000)
+            .GroupBy(c => c.Bank)
+            Select
+
+
+
+
+
+            foreach(int sq in HeresSquaredo)
+            {
+                Console.WriteLine(sq);
+            }
+            Console.WriteLine();
+
+            // Build a collection of customers who are millionaires
+
+            
+
+            List<Customer> customers = new List<Customer>() {
+                new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+                new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+                new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+                new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+                new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+                new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+                new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+                new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+                new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+                new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+            };
+
+
+
 
 
 
